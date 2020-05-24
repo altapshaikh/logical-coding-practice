2561f437c8a2419ae60dcfdbc44fe3c53fd8563a
altap shaikh <altap_shaikh111@rediffmail.com>
2020-05-24 10:21:32 +0530

    new class git practice

--- src/com/learnbyheart/GitPractice.java
+++ src/com/learnbyheart/GitPractice.java
@@ -3,7 +3,10 @@ package com.learnbyheart;
 public class GitPractice {
 	
 	public static void main(String[] args) {
-		
+		int a=5;
+		int b=10;
+		int c=a+b;
+		System.out.println("sum of number is:"+c);
 	}
 
 }
